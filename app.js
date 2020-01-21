c96e524b2a6be667347e7399c30b534f51a0aa93
AkramSultani <akram.nadri@gmail.com>
2020-01-21 12:45:43 -0500

    test 123

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-# test
-# new test 123
-# test 3 commit
+'use strict'
+
+let today = new Date()
+let formatDate = today.toDateString()
+let selectElement = document.getElementById('date')
+selectElement.innerHTML = formatDate
+
+console.log('Here\'s a hidden message')
