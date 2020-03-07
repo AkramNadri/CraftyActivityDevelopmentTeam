bce3e6a5eb17abb701e41dcdfe3dc5e2b5e73554
AkramSultani <akram.nadri@gmail.com>
2020-03-07 12:58:49 -0500

    removed app.js file

--- app.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-let today = new Date()
-let formatDate = today.toDateString()
-let selectElement = document.getElementById('date')
-selectElement.innerHTML = formatDate
-
-console.log('Here\'s a hidden message')
